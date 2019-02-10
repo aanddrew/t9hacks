3fd7cc8cc56623c55171b1a513ca4dcd539708c7
Vikram-T <vikram.tholakapalli@gmail.com>
2019-02-09 17:48:28 -0700

    Modified readEmail.py into object oriented

--- API_Stuff/readEmail.py
+++ API_Stuff/readEmail.py
@@ -7,10 +7,19 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import base64
 import json
+from apiclient import errors
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
-def main():
+
+class readEmailMaster:
+  def __init__(self):
+    self.service = 0
+    self.SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+  def decodeBase64(self,email64):
+      emailASCII = base64.b64decode(email64)
+      print(emailASCII)
+
+  def Authentication(self):
     """Shows basic usage of the Gmail API.
     Lists the user's Gmail labels.
     """
@@ -26,70 +35,55 @@ def main():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', self.SCOPES)
             creds = flow.run_local_server()
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
-    service = build('gmail', 'v1', credentials=creds)
+    self.service = build('gmail', 'v1', credentials=creds)
 
-    # Call the Gmail API
-    msgs = ListMessagesMatchingQuery(service, "me")
+  def ListMessagesMatchingQuery(self, service, user_id):
+    """List all Messages of the user's mailbox matching the query.
+
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      query: String used to filter messages returned.
+      Eg.- 'from:user@some_domain.com' for Messages from a particular sender.
+
+    Returns:
+      List of Messages that match the criteria of the query. Note that the
+      returned list contains Message IDs, you must use get with the
+      appropriate ID to get the details of a Message.
+    """
+    try:
+      response = service.users().messages().list(userId=user_id).execute()
+      messages = []
+      if 'messages' in response:
+        messages.extend(response['messages'])
+
+      while 'nextPageToken' in response:
+        page_token = response['nextPageToken']
+        response = service.users().messages().list(userId=user_id, q=query, pageToken=page_token).execute()
+        messages.extend(response['messages'])
+
+      return messages
+    except errors.HttpError:
+      print('An error occurred: %s') % error
+
+    
+  def actuallyRead(self):
+    msgs = self.ListMessagesMatchingQuery(self.service , "me")
     for x in msgs:
         msg_ID = x['id']
-        msg= service.users().messages().get(userId='me', id=msg_ID).execute()
+        msg= self.service.users().messages().get(userId='me', id=msg_ID).execute()
         email64 = json.dumps(msg['payload']['parts'][0]['body']['data'], sort_keys=True, indent=4, separators=(',', ': '))
-        decodeBase64(email64)
-    # results = service.users().labels().list(userId='me').execute()
-    # labels = results.get('labels', [])
-    
-
-    # if not labels:
-    #     print('No labels found.')
-    # else:
-    #     print('Labels:')
-    #     for label in labels:
-    #         print(label['name'])
+        self.decodeBase64(email64)
     return msgs;
 
-def decodeBase64(email64):
-    emailASCII = base64.b64decode(email64)
-    print(emailASCII)
-
-from apiclient import errors
-
-
-def ListMessagesMatchingQuery(service, user_id):
-  """List all Messages of the user's mailbox matching the query.
-
-  Args:
-    service: Authorized Gmail API service instance.
-    user_id: User's email address. The special value "me"
-    can be used to indicate the authenticated user.
-    query: String used to filter messages returned.
-    Eg.- 'from:user@some_domain.com' for Messages from a particular sender.
-
-  Returns:
-    List of Messages that match the criteria of the query. Note that the
-    returned list contains Message IDs, you must use get with the
-    appropriate ID to get the details of a Message.
-  """
-  try:
-    response = service.users().messages().list(userId=user_id).execute()
-    messages = []
-    if 'messages' in response:
-      messages.extend(response['messages'])
-
-    while 'nextPageToken' in response:
-      page_token = response['nextPageToken']
-      response = service.users().messages().list(userId=user_id, q=query,
-                                         pageToken=page_token).execute()
-      messages.extend(response['messages'])
-
-    return messages
-  except (errors.HttpError, error):
-    print('An error occurred: %s') % error
-
 if __name__ == '__main__':
-    main();
+    yeet = readEmailMaster()
+    yeet.Authentication()
+    yeet.actuallyRead()
